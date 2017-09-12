9c741b8ba3150f1d5f487ffbf1112709e24153e7
Bruno Michel <bmichel@menfin.info>
2017-09-12 16:24:00 +0200

    Allow to search contacts

--- web/search/search.go
+++ web/search/search.go
@@ -5,16 +5,15 @@ import (
 	"net/http"
 
 	"github.com/blevesearch/bleve"
+	"github.com/cozy/cozy-stack/pkg/consts"
 	"github.com/cozy/cozy-stack/pkg/couchdb"
 	"github.com/cozy/cozy-stack/pkg/instance"
 	"github.com/cozy/cozy-stack/web/middlewares"
 	"github.com/labstack/echo"
 )
 
-func getIndex(ins *instance.Instance, doctype string) (bleve.Index, error) {
-	if doctype != "io.cozy.files" {
-		return nil, errors.New("Only io.cozy.files can be searched currently")
-	}
+func getFilesIndex(ins *instance.Instance) (bleve.Index, error) {
+	doctype := consts.Files
 	nameFieldMapping := bleve.NewTextFieldMapping()
 	nameFieldMapping.Analyzer = "en"
 	docMapping := bleve.NewDocumentMapping()
@@ -30,7 +29,7 @@ func getIndex(ins *instance.Instance, doctype string) (bleve.Index, error) {
 	idx.SetName(doctype)
 
 	var docs []map[string]interface{}
-	req := &couchdb.AllDocsRequest{Limit: 100} // TODO index all docs
+	req := &couchdb.AllDocsRequest{Limit: 1000} // TODO index all docs
 	if err = couchdb.GetAllDocs(ins, doctype, req, &docs); err != nil {
 		return nil, err
 	}
@@ -42,6 +41,45 @@ func getIndex(ins *instance.Instance, doctype string) (bleve.Index, error) {
 	}
 
 	return idx, nil
+}
+
+func getContactsIndex(ins *instance.Instance) (bleve.Index, error) {
+	doctype := consts.Contacts
+	docMapping := bleve.NewDocumentMapping()
+	docMapping.DefaultAnalyzer = "en"
+	mapping := bleve.NewIndexMapping()
+	mapping.DefaultType = doctype
+	mapping.AddDocumentMapping(doctype, docMapping)
+
+	idx, err := bleve.NewMemOnly(mapping) // TODO in-memory is just for testing
+	if err != nil {
+		return nil, err
+	}
+	idx.SetName(doctype)
+
+	var docs []map[string]interface{}
+	req := &couchdb.AllDocsRequest{Limit: 1000} // TODO index all docs
+	if err = couchdb.GetAllDocs(ins, doctype, req, &docs); err != nil {
+		return nil, err
+	}
+
+	for _, doc := range docs {
+		if id, ok := doc["_id"].(string); ok {
+			idx.Index(id, doc)
+		}
+	}
+
+	return idx, nil
+}
+
+func getIndex(ins *instance.Instance, doctype string) (bleve.Index, error) {
+	switch doctype {
+	case consts.Files:
+		return getFilesIndex(ins)
+	case consts.Contacts:
+		return getContactsIndex(ins)
+	}
+	return nil, errors.New("Only io.cozy.files and io.cozy.contacts can be searched currently")
 }
 
 // curl "http://cozy.tools:8080/search/io.cozy.files?q=Demo" | jq .
@@ -61,6 +99,7 @@ func search(c echo.Context) error {
 
 	query := bleve.NewQueryStringQuery(q)
 	request := bleve.NewSearchRequest(query)
+	request.Fields = []string{"*"}
 	results, err := idx.Search(request)
 	if err != nil {
 		return err
